034902a9297c990c1b78d956650e88fa4ba028d1
NicolasBoni <rm551965@fiap.com.br>
2023-08-21 10:04:27 -0300

    reinicalizando o app.jsx

---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+
+export default function Menu() {
+  return (
+    <ul>
+        <li>fiap</li>
+        <li>google</li>
+        <li>facebook</li>
+        <li>tiwitter</li>
+    
+    </ul>
+  )
+}
